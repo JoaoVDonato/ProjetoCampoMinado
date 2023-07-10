e2ab23a4bd42fd32dc238dfed087eb532ac7d9a8
João Donato <joaovictor_gd@hotmail.com>
2023-07-10 09:30:10 -0300

    Version 1.1.1

--- src/br/com/cod3r/cm/Aplicacao.java
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -3,9 +3,6 @@ package br.com.cod3r.cm;
 import br.com.cod3r.cm.modelo.Tabuleiro;
 import br.com.cod3r.cm.visao.TabuleiroConsole;
 
-
-
-
 public class Aplicacao {
 
         public static void main(String[] args) {
